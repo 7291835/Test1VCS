c8c5e0643fe54aa514a2f7823b629bb117186830
7291835 <ph.chawanwich_st@tni.ac.th>
2017-02-03 03:09:44 +0000

    commit4

--- README.md
+++ README.md
@@ -47,3 +47,4 @@ To watch some training videos, visit http://www.youtube.com/user/c9ide.# Test1VC
 # Test1VCS
 # C9
 # C9
+# C9
--- SRC/generate_list.py
+++ SRC/generate_list.py
@@ -1,17 +1,18 @@
 import random
-def generate_list():
-    alist = [x for x in range(random.randint(-10,10))]
- 
- 
+a=0
+for a++;a<=1000
+    def generate_list():
+        alist = [x for x in range(random.randint(-10,10))]
+        assert len(alist)!=0
+        assert sum(alist)<100
+        return alist
     
-    return alist
+    def printIt():
+        print(generate_list())
     
-def printIt():
-    print(generate_list())
+    def main():
+        printIt()
     
-def main():
-    printIt()
-    
-if __name__=='__main__':
-    print("Test printIt():")
-    main()
+    if __name__=='__main__':
+        print("Test printIt():")
+        main()
